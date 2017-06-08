9f14f0b80acd0723cc51e34e2883c3c3561b6aa9
Daniel Tomaszuk <dtomaszuk01@gmail.com>
2017-06-08 09:39:17 +0200

    Add log in and new user functions with password validation

--- gg.py
+++ gg.py
@@ -10,6 +10,86 @@ import hashlib
 import random
 import string
 
+def log_in(args):
+    # try to log in    
+    if args.password:
+        password = getpass("User: {}, password: ".format(args.user))
+  
+    if (len(password) >= 8):
+        # connect into DB
+        cnx = connect_db()        
+        cnx.autocommit = True
+        cursor = cnx.cursor() 
+        # create instance of User - get all users in DB
+        gg_users = User.load_all_users(cursor)        
+        db_checked = False
+        counter = 0
+        for gg_user in gg_users:            
+            # if there is an user to log into and his password is correct
+            if args.user == gg_user.email and check_password(password, gg_user.hashed_password()):
+                print ("Logged as {}".format(args.user)) 
+                disconnect_db(cursor, cnx)
+                # logged in  
+                return gg_user.id
+            # if there is an user to log into but password is incorrect
+            elif args.user == gg_user.email and check_password(password, gg_user.hashed_password()) == False:
+                print ("Wrong password..")
+                disconnect_db(cursor, cnx)
+                # wrong pass - log in failed
+                return False            
+            counter+=1
+            # if whole DB was checked with no match - set flag db_checked, create new user
+            if counter == len(gg_users):
+                db_checked = True            
+         
+           
+        if db_checked:
+            new_gg_user = User()
+            new_gg_user.email = args.user
+            new_gg_user.hashed_password = password_hash(password)
+            # allow user to choose his username
+            new_gg_user.username = input("Creating new user.\n Write your username: ")            
+            new_gg_user.save_to_db(cursor)
+            print("New user made..")
+            disconnect_db(cursor, cnx)
+            # new user and logged in
+            return new_gg_user.id
+    else:
+        print ("Password too short..")
+        disconnect_db(cursor, cnx)
+        # log in failed
+        return False
+
+
+def new_pass(args):
+    # set new pass only if proper -u and -p were given as well as -n 
+    log_in_result = log_in(args)   
+    if log_in_result != False and args.new_pass:
+            new_pass = getpass("Set new password: ")  
+            if len(new_pass) >= 8:
+                cnx = connect_db()        
+                cnx.autocommit = True
+                cursor = cnx.cursor()
+                user = User.load_by_id(log_in_result, cursor) 
+                # set new password           
+                user.hashed_password = password_hash(new_pass)
+                # update new password in DB
+                sql = """UPDATE Users SET hashed_password = '{}'
+                         WHERE id = {}""".format(user.hashed_password, user.id)            
+                cursor.execute(sql)
+                disconnect_db(cursor,cnx)
+                print(user.email)              
+                print ("New password set!")
+                return user.id
+            else:
+                print("Password too short..")
+                return False
+
+# def delete(args):
+#     log_in_result = log_in(args)
+#     if log_in_result != False and args.new_pass: 
+
+
 
 def main():
     
@@ -18,7 +98,7 @@ def main():
     group = parser.add_mutually_exclusive_group()
     
     # while logging
-    parser.add_argument("-u","--user", help="user login set as email!.")
+    parser.add_argument("-u","--user", help="user login, email in DB")
     # parser.add_argument("-p","--password", help="user password")
     
     parser.add_argument('-p', '--password', action='store_true', dest='password', 
@@ -33,68 +113,25 @@ def main():
     
      
     # after succesful log
-    parser.add_argument("-l","--list", action="store_true", default=0, help="list all user emails")
-    parser.add_argument("-d","--delete", action="store_true", default=0, help="delete logged user")
-    parser.add_argument("-e","--edit", action="store_true", default=0, help="edit user email")
+    parser.add_argument("-l","--list", action="store_true", help="list all user emails")
+    parser.add_argument("-d","--delete", action="store_true", help="delete logged user")
+    parser.add_argument("-e","--edit", action="store_true", help="edit user email")
     
     
     group.add_argument("-v", "--verbose", action="store_true", help = "more verbose output")
     group.add_argument("-q", "--quiet", action="store_true", help = "stays quiet")
-    parser.add_argument("--quit", action="store_false", default=1, help="closes the program")
-    
-    
+    parser.add_argument("--quit", action="store_false", help="closes the program")
+        
     args = parser.parse_args()
-    if args.password:
-            password = getpass("User: {}, password: ".format(args.user))
-            
-            
-    if args.new_pass:
-            new_pass = getpass("Set new password: ")
     
     
-    
-# try to log in    
-    if (len(password) >= 8):
-        
-        # connect into DB
-        cnx = connect_db()        
-        cnx.autocommit = True
-        cursor = cnx.cursor() 
-        # create instance of User - get all users in DB
-        gg_users = User.load_all_users(cursor)
-        
-        
-        db_checked = False
-        counter = 0
-        for gg_user in gg_users:            
-            # if there is an user to log into and his password is correct
-            if args.user == gg_user.email and check_password(password, gg_user.hashed_password()):
-                print ("Logging in as {}".format(args.user))   
-                break 
-            # if there is an user to log into but password is incorrect
-            elif args.user == gg_user.email and check_password(password, gg_user.hashed_password()) == False:
-                print ("Wrong password..")
-                break            
-            counter+=1
-            # if whole DB was checked with no match - set flag db_checked, create new user
-            if counter == len(gg_users):
-                db_checked = True            
-            
-        if db_checked:
-            new_gg_user = User()
-            new_gg_user.email = args.user
-            new_gg_user.hashed_password = password_hash(password)
-            # allow user to choose
-            new_gg_user.username = input("Creating new user.\n Write your username: ")
-            
-            new_gg_user.save_to_db(cursor)
-            
-         
-        disconnect_db(cursor, cnx)
-#        
-    else:
-        print ("Password too short..")
-    
+    # log in or create new user   
+    if (args.user is not None) and (args.new_pass is None):
+        log_in(args)
+    elif (args.user is not None) and (args.new_pass is not None):
+        new_pass(args)
+    # delete user after loggin in
+#     if (args.user is not None) and (args.delete is not None):
     
     
 
